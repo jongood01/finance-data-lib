51aa11eb0edf25f7b668f2b1daa1ee3cb7512d2e
jongood01 <jongood01@gmail.com>
2023-05-25 19:22:49 +1200

    Add login req and res

--- src/auth/login.schema.ts
+++ src/auth/login.schema.ts
@@ -1,3 +1,8 @@
-export type LoggedInResponse = {
+import { z } from "../deps.ts";
+import { CredentialsSchema } from "./credentials.schema.ts";
+
+export type LoginResponse = {
   token: string;
 };
+
+export type LoginRequest = z.infer<typeof CredentialsSchema>;
