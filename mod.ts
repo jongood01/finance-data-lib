4938f486788110f37e2c18341c7c40042798c851
jongood01 <jongood01@gmail.com>
2023-07-03 19:51:12 +1200

    Fix list req and response export

--- mod.ts
+++ mod.ts
@@ -1,4 +1,8 @@
 export * from "./src/accounts/index.ts";
 export * from "./src/auth/index.ts";
 export * from "./src/user/index.ts";
-export { type CreatedByUserInfo } from "./src/core/index.ts";
+export type {
+  CreatedByUserInfo,
+  ListRequest,
+  ListResponse,
+} from "./src/core/index.ts";
