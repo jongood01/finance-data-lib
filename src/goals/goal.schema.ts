6bb1e972fc2cb536b903c0bf9bd1b4bf2db2c3f4
jongood01 <jongood01@gmail.com>
2024-05-31 17:48:46 +1200

    Updated goal schema

--- src/goals/goal.schema.ts
+++ src/goals/goal.schema.ts
@@ -24,14 +24,18 @@ const goalBase = {
     .number()
     .nonnegative()
     .max(999999)
-    .step(0.01, { message: "Complete amount must be a valid decimal" }),
+    .step(0.01, { message: "Complete amount must be a valid decimal" })
+    .nullable(),
   completeDate: z
     .string()
-    .datetime({ message: "Complete date at must be a valid date string" }),
-  budgetId: z.string({
-    required_error: "Budget id is required",
-    invalid_type_error: "Budget id needs to be a valid string",
-  }),
+    .datetime({ message: "Complete date at must be a valid date string" })
+    .nullable(),
+  budgetId: z
+    .string({
+      required_error: "Budget id is required",
+      invalid_type_error: "Budget id needs to be a valid string",
+    })
+    .nullable(),
   type,
 };
 
