62e7b1c448bc7d70eedb9c008621fbab0182f4d1
jongood01 <jongood01@gmail.com>
2024-06-03 08:48:31 +1200

    Added  create/update goal request schema and types

--- src/goals/goal.schema.ts
+++ src/goals/goal.schema.ts
@@ -9,7 +9,7 @@ const type = z.enum([
 ]);
 const amountType = z.enum(["$", "%"]);
 
-const goalBase = {
+const goalExternalBase = {
   amount: z
     .number()
     .nonnegative()
@@ -19,17 +19,6 @@ const goalBase = {
   goalDate: z
     .string()
     .datetime({ message: "Goal date at must be a valid date string" }),
-  complete: z.boolean(),
-  completeAmount: z
-    .number()
-    .nonnegative()
-    .max(999999)
-    .step(0.01, { message: "Complete amount must be a valid decimal" })
-    .nullable(),
-  completeDate: z
-    .string()
-    .datetime({ message: "Complete date at must be a valid date string" })
-    .nullable(),
   budgetId: z
     .string({
       required_error: "Budget id is required",
@@ -39,7 +28,36 @@ const goalBase = {
   type,
 };
 
+const goalBase = {
+  ...goalExternalBase,
+  goalDate: z.date({
+    required_error: "Goal date must be a valid date",
+  }),
+  complete: z.boolean(),
+  completeAmount: z
+    .number()
+    .nonnegative()
+    .max(999999)
+    .step(0.01, { message: "Complete amount must be a valid decimal" })
+    .nullable(),
+  completeDate: z
+    .date({ required_error: "Complete date at must be a valid date" })
+    .nullable(),
+};
+
 export const GoalSchema = EntitySchema.extend(goalBase).strict();
+export const CreateGoalRequestSchema = z.object(goalExternalBase).strict();
+export const UpdateGoalRequestSchema = z
+  .object({
+    ...goalExternalBase,
+    id: z.string({
+      required_error: "Goal id is required",
+      invalid_type_error: "Goal id must be a string",
+    }),
+  })
+  .strict();
 type RawGoal = z.infer<typeof GoalSchema>;
 export type Goal = RawGoal & WithObjectId;
 export type GoalType = z.infer<typeof type>;
+export type CreateGoalRequest = z.infer<typeof CreateGoalRequestSchema>;
+export type UpdateGoalRequest = z.infer<typeof UpdateGoalRequestSchema>;
